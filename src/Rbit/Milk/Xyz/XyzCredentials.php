eebccaee850aa8c2ef3d57c42b1aa151803750f5
Roberto B <roberto.butti@gmail.com>
2020-01-22 22:33:12 +0100

    Update XyzCredentials.php

--- src/Rbit/Milk/Xyz/XyzCredentials.php
+++ src/Rbit/Milk/Xyz/XyzCredentials.php
@@ -6,8 +6,6 @@ namespace Rbit\Milk\Xyz;
 
 class XyzCredentials
 {
-    private string $iv;
-    private string $tokenEncrypted;
     private string $token;
 
 
@@ -15,29 +13,13 @@ class XyzCredentials
      * Constructs a new XyzCredentials object, with the specified XYZ
      * access token
      *
-     * @param string $key     AWS access key ID
-     * @param string $secret  AWS secret access key
      * @param string $token   Security token to use
      */
-    public function __construct($token = "", $iv = "", $tokenEncrypted = "")
+    public function __construct($token = "")
     {
         $this->token = $token;
-        $this->iv = $iv;
-        $this->tokenEncrypted = $tokenEncrypted;
-
-        if ($this->token === "") {
-            $token = $this->decryptToken($tokenEncrypted, $iv);
-        }
-
     }
 
-
-
-    public function decryptToken($tokenEncrypted, $iv) {
-
-        $retVal = openssl_decrypt($tokenEncrypted, AES_256_CBC, $encryption_key, 0, $iv);
-        return $retVal;
-    }
     public static function __set_state(array $state)
     {
         return new self(
@@ -54,22 +36,30 @@ class XyzCredentials
         $this->token = $token;
     }
 
-    public function toArray()
+    /**
+     * Return the Array representation of this object
+     * [
+     *   'token' => 'some token'
+     * ]
+     *
+     * @return array
+     */
+    public function toArray(): array
     {
         return [
             'token'     => $this->token
         ];
     }
+
     public function serialize()
     {
         return json_encode($this->toArray());
     }
+
     public function unserialize($serialized)
     {
         $data = json_decode($serialized, true);
         $this->token = $data['token'];
     }
-
-
 
 }
