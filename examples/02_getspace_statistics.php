04348d785c3b76e9933a5a48c6a1318d62686f9b
Roberto B <roberto.butti@gmail.com>
2020-01-26 16:54:11 +0100

    Create 02_getspace_statistics.php

---
+++ examples/02_getspace_statistics.php
@@ -0,0 +1,28 @@
+<?php
+require __DIR__."/../vendor/autoload.php";
+
+use \Rbit\Milk\Xyz\XyzSpaceFeature;
+use \Rbit\Milk\Xyz\XyzConfig;
+
+function print_row($item, $key)
+{
+    echo $key+1 . " ". $item->id . " ". $item->owner . " " . $item->title."\n";
+}
+
+
+$spaceId = readline("Space ID : ");
+/** XyzSpace $xyzSpace */
+$xyzSpaceFeature = new XyzSpaceFeature(XyzConfig::getInstance());
+
+$xyzSpaceFeature->reset();
+
+$o1 = $xyzSpaceFeature->spaceId($spaceId)->statistics()->get();
+
+
+echo "The Spaces has {$o1->count->value} features ".PHP_EOL;
+echo $o1->count->estimated ? "The count is estimated" : "The count is real:)";
+echo PHP_EOL;
+
+echo "The Spaces is {$o1->byteSize->value} byte ".PHP_EOL;
+echo $o1->byteSize->estimated ? "The size is estimated" : "The size is real:)";
+echo PHP_EOL;
