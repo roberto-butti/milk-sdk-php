6b957d4bcea7429a86e94f722a9a32730a754f74
Roberto B <roberto.butti@gmail.com>
2020-01-27 22:12:16 +0100

    echo features object

--- examples/03_iteratefeatures.php
+++ examples/03_iteratefeatures.php
@@ -1,16 +1,20 @@
 <?php
-require __DIR__."/../vendor/autoload.php";
+
+/**
+ * /spaces/{spaceId}/iterate
+ * Iterates all of the features in the space. The features in the response are ordered so that no feature is returned twice.
+ * https://xyz.api.here.com/hub/static/swagger/#/Read%20Features/iterateFeatures
+ *
+ */
+require __DIR__ . "/../vendor/autoload.php";
 
 use \Rbit\Milk\Xyz\XyzSpaceFeature;
 use \Rbit\Milk\Xyz\XyzConfig;
+use \Rbit\Milk\Utils\Obj;
 
 function print_row($item, $key)
 {
-    foreach ($item->properties  as $key => $value) {
-        if (gettype($value) == "string") {
-            echo "$key => $value" . PHP_EOL;
-        }
-    }
+    Obj::echo($item);
     echo "------------------------" . PHP_EOL;
 }
 
@@ -21,4 +25,3 @@ $xyzSpaceFeature = new XyzSpaceFeature(XyzConfig::getInstance());
 $result = $xyzSpaceFeature->iterate($spaceId)->get();
 array_walk($result->features, 'print_row');
 echo PHP_EOL;
-
