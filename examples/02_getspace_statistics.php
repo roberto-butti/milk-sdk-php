103e69b0d248c58ff88deed2f610239c0f87eeb5
Roberto B <roberto.butti@gmail.com>
2020-01-27 22:12:42 +0100

    formatting code :nail_care:

--- examples/02_getspace_statistics.php
+++ examples/02_getspace_statistics.php
@@ -1,12 +1,12 @@
 <?php
-require __DIR__."/../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 use \Rbit\Milk\Xyz\XyzSpace;
 use \Rbit\Milk\Xyz\XyzConfig;
 
 function print_row($item, $key)
 {
-    echo $key+1 . " ". $item->id . " ". $item->owner . " " . $item->title."\n";
+    echo $key + 1 . " " . $item->id . " " . $item->owner . " " . $item->title . "\n";
 }
 
 
@@ -18,10 +18,10 @@ $xyzSpace->reset();
 $o1 = $xyzSpace->spaceId($spaceId)->statistics()->get();
 
 
-echo "The Spaces has {$o1->count->value} features ".PHP_EOL;
+echo "The Spaces has {$o1->count->value} features " . PHP_EOL;
 echo $o1->count->estimated ? "The count is estimated" : "The count is real:)";
 echo PHP_EOL;
 
-echo "The Spaces is {$o1->byteSize->value} byte ".PHP_EOL;
+echo "The Spaces is {$o1->byteSize->value} byte " . PHP_EOL;
 echo $o1->byteSize->estimated ? "The size is estimated" : "The size is real:)";
 echo PHP_EOL;
