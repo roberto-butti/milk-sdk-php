22894268ede154e0f6ee83d9121b881a9d315274
Roberto B <roberto.butti@gmail.com>
2020-02-01 19:28:21 +0100

    Some Test for Space Features

--- tests/e2e/XyzSpaceFeatureTest.php
+++ tests/e2e/XyzSpaceFeatureTest.php
@@ -8,7 +8,20 @@ class XyzSpaceFeatureTest extends TestCase
     public function testGetListSpaces()
     {
         $xyzSpaceFeature = XyzSpaceFeature::instance()->iterate("bB6WZ2Sb")->getResponse();
-
         $this->assertEquals(200, $xyzSpaceFeature->getStatusCode(), "Testing List Feature");
+        $xyzSpaceFeature = XyzSpaceFeature::instance()->iterate("bB6WZ2Sb")->get();
+        $this->assertEquals("FeatureCollection", $xyzSpaceFeature->type, "Check FeatureCollection");
+        $this->assertIsArray( $xyzSpaceFeature->features, "Check Features");
+        $this->assertGreaterThan( 1, count($xyzSpaceFeature->features) , "Check lenght Features");
+        //$feature = array_rand($xyzSpaceFeature->features, 1);
+        $feature = $xyzSpaceFeature->features[0];
+        for ($i=0; $i < count($xyzSpaceFeature->features); $i++) {
+            $feature = $xyzSpaceFeature->features[$i];
+            $this->assertEquals("Feature", $feature->type, "Check Feature type");
+            $this->assertObjectHasAttribute("geometry", $feature, "Check Geometry");
+            $this->assertObjectHasAttribute("properties", $feature, "Check Properties");
+
+        }
+
     }
 }
