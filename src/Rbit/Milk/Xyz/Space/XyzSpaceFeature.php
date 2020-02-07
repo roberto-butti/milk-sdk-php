878d8bb72672b7f296c864d3da987c8f7858e823
Roberto B <roberto.butti@gmail.com>
2020-02-07 21:07:32 +0100

    Parameters for iterate features endpoint

--- src/Rbit/Milk/Xyz/Space/XyzSpaceFeature.php
+++ src/Rbit/Milk/Xyz/Space/XyzSpaceFeature.php
@@ -14,6 +14,13 @@ use Rbit\Milk\Xyz\Common\XyzClient;
 class XyzSpaceFeature extends XyzClient
 {
     private string $featureId ="";
+    private ?int $paramLimit = null;
+    private array $paramSelection = [];
+    private bool $paramSkipCache = false;
+    private string $paramHandle = "";
+
+
+
 
 
     public function __construct()
@@ -41,14 +48,22 @@ class XyzSpaceFeature extends XyzClient
     public function reset()
     {
         parent::reset();
+
         $this->contentType = "application/geo+json";
         $this->featureId = "";
         $this->spaceId = "";
+        $this->paramLimit = null;
+        $this->paramSelection = [];
+        $this->paramSkipCache = false;
+        $this->paramHandle = "";
+
+
     }
 
     public function iterate($spaceId): XyzSpaceFeature
     {
         $this->spaceId = $spaceId;
+        $this->contentType = "application/geo+json";
         $this->setType(self::API_TYPE_ITERATE);
         return $this;
     }
@@ -64,13 +79,69 @@ class XyzSpaceFeature extends XyzClient
         return $this;
     }
 
+    /**
+     * Set the limit in the API
+     * @param int $limit
+     * @return $this
+     */
+    public function limit(int $limit): XyzSpaceFeature
+    {
+        $this->paramLimit = $limit;
+        return $this;
+    }
+
     protected function queryString(): string
     {
         $retString = "";
 
-
+        if ($this->paramSkipCache) {
+            $retString = $this->addQueryParam($retString, "skipCache", "true");
+        }
+        if ($this->paramLimit) {
+            $retString = $this->addQueryParam($retString, "limit", $this->paramLimit);
+        }
+        if ($this->paramHandle) {
+            $retString = $this->addQueryParam($retString, "handle", $this->paramHandle);
+        }
+        if (is_array($this->paramSelection) && count($this->paramSelection) > 0 ) {
+            $retString = $this->addQueryParam($retString, "selection", implode(",", $this->paramSelection));
+        }
 
         return $retString;
+    }
+
+
+    /**
+     * If set to true the response is not returned from cache. Default is false.
+     * @return $this
+     */
+    public function skipCache(bool $skipcache = true): XyzSpaceFeature
+    {
+        $this->paramSkipCache = $skipcache;
+        return $this;
+    }
+
+    /**
+     * List the properties you want to include in the response. If you have a property "title" and "description" you need to
+     * use ["p.title", "p.description"].
+     * @return $this
+     */
+    public function selection(array $propertiesList ): XyzSpaceFeature
+    {
+        $this->paramSelection = $propertiesList;
+        return $this;
+    }
+
+
+    /**
+     * List the properties you want to include in the response. If you have a property "title" and "description" you need to
+     * use ["p.title", "p.description"].
+     * @return $this
+     */
+    public function handle(string $handle ): XyzSpaceFeature
+    {
+        $this->paramHandle = $handle;
+        return $this;
     }
 
 }
--- tests/e2e/XyzSpaceFeatureTest.php
+++ tests/e2e/XyzSpaceFeatureTest.php
@@ -8,7 +8,7 @@ class XyzSpaceFeatureTest extends TestCase
     public function testGetFeaturesStatusCode()
     {
         $xyzToken = getenv('XYZ_ACCESS_TOKEN');
-        $xyzSpaceFeature = XyzSpaceFeature::instance($xyzToken)->iterate("bB6WZ2Sb")->getResponse();
+        $xyzSpaceFeature = XyzSpaceFeature::setToken($xyzToken)->iterate("bB6WZ2Sb")->getResponse();
         $this->assertEquals(200, $xyzSpaceFeature->getStatusCode(), "Testing List Feature");
     }
     public function testGetFeaturesStructure()
@@ -29,4 +29,46 @@ class XyzSpaceFeatureTest extends TestCase
         //}
 
     }
+    public function testGetFeaturesLimit()
+    {
+        $xyzToken = getenv('XYZ_ACCESS_TOKEN');
+        $xyzSpaceFeature = XyzSpaceFeature::instance($xyzToken)->iterate("bB6WZ2Sb")->limit(2)->get();
+        $this->assertEquals("FeatureCollection", $xyzSpaceFeature->type, "Check FeatureCollection");
+        $this->assertIsArray( $xyzSpaceFeature->features, "Check Features");
+        $this->assertEquals( 2, count($xyzSpaceFeature->features) , "Check lenght Features is 2");
+
+        //$feature = array_rand($xyzSpaceFeature->features, 1);
+        $feature = $xyzSpaceFeature->features[0];
+
+        //for ($i=0; $i < count($xyzSpaceFeature->features); $i++) {
+            //$feature = $xyzSpaceFeature->features[$i];
+            $this->assertEquals("Feature", $feature->type, "Check Feature type");
+            $this->assertObjectHasAttribute("geometry", $feature, "Check Geometry");
+            $this->assertObjectHasAttribute("properties", $feature, "Check Properties");
+
+        //}
+
+    }
+
+    public function testGetFeaturesLimitAndSelection()
+    {
+        $xyzToken = getenv('XYZ_ACCESS_TOKEN');
+        $xyzSpaceFeature = XyzSpaceFeature::instance($xyzToken)->iterate("bB6WZ2Sb")->limit(2)->selection(["p.loc", "p.time"])->get();
+        $this->assertEquals("FeatureCollection", $xyzSpaceFeature->type, "Features, Limit and Selection: Check FeatureCollection");
+        $this->assertIsArray( $xyzSpaceFeature->features, "Feattures, Limit and Selection: Check Features is array");
+        $this->assertEquals( 2, count($xyzSpaceFeature->features) , "Check lenght Features is 2");
+
+        //$feature = array_rand($xyzSpaceFeature->features, 1);
+        $feature = $xyzSpaceFeature->features[0];
+
+        //for ($i=0; $i < count($xyzSpaceFeature->features); $i++) {
+            //$feature = $xyzSpaceFeature->features[$i];
+            $this->assertEquals("Feature", $feature->type, "Check Feature type");
+            $this->assertObjectHasAttribute("geometry", $feature, "Check Geometry");
+            $this->assertObjectHasAttribute("properties", $feature, "Check Properties");
+
+        //}
+
+    }
+
 }
